a93b51b8e83a344fe168018442ff5c0629791bf3
siwei xu <Siwei_xu@raytheon.com>
2020-11-25 10:40:02 -0500

    Revert "CMR-6805: Removed some obsolete code."
    This reverts commit 58f91e5c7ebb9ea19b6f9892b39640e54bd0eb62.

--- search-app/src/cmr/search/api/routes.clj
+++ search-app/src/cmr/search/api/routes.clj
@@ -5,6 +5,7 @@
    [cmr.common-app.api.health :as common-health]
    [cmr.common-app.api.routes :as common-routes]
    [cmr.common.cache :as cache]
+   [cmr.search.api.association :as association-api]
    [cmr.search.api.autocomplete :as autocomplete-api]
    [cmr.search.api.community-usage-metrics :as metrics-api]
    [cmr.search.api.concepts-lookup :as concepts-lookup-api]
@@ -15,6 +16,7 @@
    [cmr.search.api.services :as services-api]
    [cmr.search.api.tags :as tags-api]
    [cmr.search.api.tools :as tools-api]
+   [cmr.search.api.variables :as variables-api]
    [cmr.search.data.metadata-retrieval.metadata-cache :as metadata-cache]
    [cmr.search.services.health-service :as hs]
    [compojure.core :refer :all])
